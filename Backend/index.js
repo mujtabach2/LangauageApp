8cfcb3602ed01672779961a35251e79e2d189ce8
mujtabach2 <mujtabawaqas@gmail.com>
2024-09-11 14:12:47 -0400

    changed url

--- Backend/index.js
+++ Backend/index.js
@@ -7,31 +7,20 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: '*',
+  origin: true, // This allows any origin
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 200
 }));
 
 // Enable pre-flight requests for all routes
 app.options('*', cors());
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:3001'];
-    if (!origin || allowedOrigins.indexOf(origin) !== -1 || origin.endsWith('.netlify.app')) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  optionsSuccessStatus: 200
-};
-app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const baseUrl = process.env.BASE_URL || 'http://localhost:3001';
+const baseUrl = process.env.BASE_URL || 'https://localhost:3001';
 const port = process.env.PORT || 3000;
 
 app.post('/generate-chat', async (req, res) => {
@@ -40,17 +29,14 @@ app.post('/generate-chat', async (req, res) => {
     const { role, name, session_length, language, proficiency, topic, mode, starter, input } = req.body;
     console.log(role, name, session_length, language, proficiency, topic, mode, starter, input);
 
-    // Assuming GPTChatWrapper is an ES module
     const gpt_chat_wrapper = new GPTChatWrapper('User', name, session_length, language, proficiency, topic, mode, starter, input);
     const response = await gpt_chat_wrapper.run();
 
     if (response === null) {
-      res.status(500).json({ error: 'shii not working' });
-    }else{
-      res.status(200).json({ chat: response.text });
+      return res.status(500).json({ error: 'Internal Server Error' });
     }
-    // Return the response directly
-    res = response.text;
+    
+    res.status(200).json({ chat: response.text });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
